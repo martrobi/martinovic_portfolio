5bd7262df0eda8975418d626dea2a88344879ace
robert_martinovic <martinovicrobert2001@gmail.com>
2023-09-23 21:31:08 +0200

    ispravci responsivni dizajn

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import Skills from './Skills';
 import About from './About';
 import Contact from './Contact';
@@ -8,30 +8,39 @@ import laptopImage from './laptop.png';
 
 function App() {
   const [activeComponent, setActiveComponent] = useState(null);
+  const [isFloating2Hidden, setIsFloating2Hidden] = useState(false);
 
   const handleNavItemClick = (componentName) => {
     setActiveComponent(componentName);
+    
+    if (componentName === 'Skills' || componentName === 'About' || componentName === 'Contact') {
+      setIsFloating2Hidden(true);
+    } else {
+      setIsFloating2Hidden(false);
+    }
   };
 
   return (
     <div>
       <div>
-      <NavMenu onNavItemClick={handleNavItemClick} />
-      {activeComponent === 'Skills' && <Skills />}
-      {activeComponent === 'About' && <About />}
-      {activeComponent === 'Contact' && <Contact />}
+        <NavMenu onNavItemClick={handleNavItemClick} />
+        {activeComponent === 'Skills' && <Skills />}
+        {activeComponent === 'About' && <About />}
+        {activeComponent === 'Contact' && <Contact />}
       </div>
+      {!isFloating2Hidden && (
+        <div className='floating2'><img className='sphere2' src={laptopImage} alt='Floating' /></div>
+      )}
       <div className='floating'><img className='sphere' src={laptopImage} alt='Floating' /></div>
-      <div className='floating2'><img className='sphere2' src={laptopImage} alt='Floating' /></div>
-      <div  className='text-mobile'>
+      <div className='text-mobile'>
         <p className='text-only'><span style={{ color: '#1bfaff' }}>Welcome,</span><br/>
         to my portfolio<br/>
         <div style={{display: 'flex', justifyContent: 'center', paddingTop: '15px'}}><span style={{ color: '#1bfaff', position: 'center', fontSize: '15px' }}>-Robert Martinovic-</span></div>
         </p>
-        
       </div>
     </div>
   );
 }
 
 export default App;
+
